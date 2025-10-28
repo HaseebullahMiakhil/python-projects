8c088eaca59eacc2b714728862030807f33947b6
HaseebullahMiakhil <tw773029@gmail.com>
2025-10-28 08:04:53 +0430

    loops

---
+++ for_loops.py
@@ -0,0 +1,10 @@
+# password validation example
+
+for mynumber in range(10):
+    password = input('enter your number: ')
+    if password == 'miakhil' :
+        break
+    else :
+        print('enter right password')
+
+print('for loop ended')
