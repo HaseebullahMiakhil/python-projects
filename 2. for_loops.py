c279ed9aff9a60b2ae26c45d4e473902e087b505
HaseebullahMiakhil <tw773029@gmail.com>
2025-10-28 09:05:48 +0430

    while

--- 2. for_loops.py
+++ 2. for_loops.py
@@ -2,4 +2,8 @@ user_input = ""
 
 while user_input != 'end' :
     user_input = input(">>")
+    if user_input == 'n':
+        continue
+    elif user_input == 'j':
+        break
     print(user_input)
