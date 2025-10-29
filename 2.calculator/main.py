1ea0b8467f15fd161e2145e86bd8d77b1992a45b
HaseebullahMiakhil <tw773029@gmail.com>
2025-10-29 14:01:36 +0430

    3

--- 2.calculator/main.py
+++ 2.calculator/main.py
@@ -1,27 +1,32 @@
 from tkinter import Tk, Entry, Button, StringVar
 
 class Calculator:
-    def __init__(self):
-        master.title("Calculator")
-        master.geometry("357x420+0+0")
-        master.config(bg='gray')
-        master.resizable(False, False)
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Calculator")
+        self.master.geometry("357x420+0+0")
+        self.master.config(bg='gray')
+        self.master.resizable(False, False)
 
-        self.equation=StringVar()
+        self.equation = StringVar()
         self.entry_value = ""
-        Entry(width=17, bg='fff', font=('Arial Bold',28), textvariable=self.equation).place(x=0, y=0)
+        Entry(self.master, width=17, bg='white', font=('Arial Bold', 28), textvariable=self.equation).place(x=0, y=0)
 
     def show(self, value):
-        self.entry_value+=str(value)
+        self.entry_value += str(value)
         self.equation.set(self.entry_value)
 
     def clear(self):
-        self.entry_value=""
+        self.entry_value = ""
         self.equation.set(self.entry_value)
 
     def solve(self):
-        result = eval(self.entry_value)
-        self.equation.set(result)
-root=Tk()
+        try:
+            result = eval(self.entry_value)
+            self.equation.set(result)
+        except Exception as e:
+            self.equation.set("Error")
+
+root = Tk()
 calculator = Calculator(root)
 root.mainloop()
